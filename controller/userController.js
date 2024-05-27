b32f1d88ecd90bb79b17ed9b2c5f7821b119599f
Prasad Patil <patilprasadbharat@gmail.com>
2024-05-27 09:54:07 +0530

    create New Admin Controller

--- controller/userController.js
+++ controller/userController.js
@@ -79,3 +79,43 @@ export const login= catchAsyncErrors(async(req,resp,next)=>{
   generateToken(user,"User LogIn Successfully",200,resp)
  
 })
+
+
+export const addNewAdmin = catchAsyncErrors(async (req, resp, next) => {
+  const { firstName, lastName, email, phone, nic, dob, gender, password } =
+    req.body;
+  if (
+    !firstName ||
+    !lastName ||
+    !email ||
+    !phone ||
+    !nic ||
+    !dob ||
+    !gender ||
+    !password
+  ) {
+    return next(new ErrorHandler("Please Fill Full Form!", 400));
+  }
+
+  const isRegistered = await User.findOne({ email });
+  if (isRegistered) {
+    return next(new ErrorHandler("Admin With This Email Already Exists!", 400));
+  }
+
+  const admin = await User.create({
+    firstName,
+    lastName,
+    email,
+    phone,
+    nic,
+    dob,
+    gender,
+    password,
+    role: "Admin",
+  });
+  resp.status(200).json({
+    success: true,
+    message: "New Admin Registered",
+    admin,
+  });
+});
