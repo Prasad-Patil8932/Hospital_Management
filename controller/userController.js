b70a74087919a481ade28ca244271ee5b30022c6
Prasad Patil <patilprasadbharat@gmail.com>
2024-05-27 09:14:24 +0530

    User LogIn Controller Created

--- controller/userController.js
+++ controller/userController.js
@@ -50,3 +50,34 @@ export const patientRegister = catchAsyncErrors(async (req, resp, next) => {
 
   })
 });
+
+
+export const login= catchAsyncErrors(async(req,resp,next)=>{
+  const {email,password,confirmPassword,role}=req.body
+  if(!email|| !password ||  !confirmPassword || !role){
+    return next(new ErrorHandler("Please Provides All Fields",400))
+  }
+
+ 
+  if (password!== confirmPassword){
+    return next(new ErrorHandler("Password And Confirm Password Does not Match",400))
+  }
+
+  const user =await User.findOne({email}).select("+password")
+
+  if (!user){
+    return next(new ErrorHandler("Invalid Email or Password",400))
+  }
+
+  const isPasswordMatched=await user.compare(password)
+  if(!isPasswordMatched){
+    return next(new ErrorHandler("Invalid Email or Password",400))
+  }
+  if(role!==user.role){
+    return next(new ErrorHandler("User With This Role Not Found",400))
+  }
+  resp.status(200).json({
+    success:true,
+    message:"User LogIn Successfully"
+  })
+})
